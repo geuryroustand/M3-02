ca911882240ec999abe0eb2be8d3972ed314878f
Geury Roustand <theromantic2010@hotmail.com>
2021-06-23 08:52:37 +0200

    homework done

--- fetch.js
+++ fetch.js
@@ -1,31 +1,35 @@
 const mainDiv = document.querySelector('.main-div');
+const btnCount = document.querySelector('.btn-count');
+const btnAll = document.querySelector('.btn-all');
 
 const randerHTLM = function (datas) {
   // console.log(datas.data);
-  datas.forEach(data => {
-    console.log(data.album.title);
+  datas.forEach((data, i) => {
+    btnCount.addEventListener('click', () => {
+      // console.log(i);
+    });
 
-    //   const html = `
-    //   <div class="col px-2">
-    //   <div class="card-homepage text-center">
-    //     <div class="row">
-    //       <div class="col-12">
-    //         <img src="${}" class="card-img-top img-fluid rounded-0" alt="image" />
-    //       </div>
+    const html = `
+      <div class="col px-2">
+      <div class="card-homepage text-center">
+        <div class="row">
+          <div class="col-12">
+            <img src="${data.album.cover}" class="card-img-top img-fluid rounded-0" alt="image" />
+          </div>
+          <div class="col-12">
+            <div class="card-body text-left">
+              <h6 class="card-text">${data.title_short}</h6>
+              <p class="card-subtext">
+                ${data.title}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+      `;
 
-    //       <div class="col-12">
-    //         <div class="card-body text-left">
-    //           <h6 class="card-text">Burning Jazz-rock Fusion</h6>
-    //           <p class="card-subtext">
-    //             You'll find fiery, modern, groovy, jazz-rock
-    //           </p>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
-
-    //   `
+    mainDiv.insertAdjacentHTML('afterbegin', html);
   });
 };
 
@@ -37,10 +41,21 @@ const getMusicData = function (category) {
       'x-rapidapi-host': 'deezerdevs-deezer.p.rapidapi.com',
     },
   })
-    .then(response => response.json())
+    .then(response => {
+      // console.log(response);
+
+      if (!response.ok)
+        throw new Error(`Information not found ${response.status}`);
+      return response.json();
+    })
     .then(data => {
-      // console.log(data.data);
+      console.log(data.data);
       randerHTLM(data.data);
+
+      btnAll.addEventListener('click', e => {});
+    })
+    .catch(err => {
+      console.error('something when wrong');
     });
 };
 
